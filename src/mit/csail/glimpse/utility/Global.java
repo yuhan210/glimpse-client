4fc26dfff8158154a34bf51342f0e9ad73fe1dea
Tiffany Chen <yuhan210@gmail.com>
2014-11-30 02:52:57 -0500

    global parameter checked in

--- src/mit/csail/glimpse/utility/Global.java
+++ src/mit/csail/glimpse/utility/Global.java
@@ -7,4 +7,9 @@ import java.io.InputStreamReader;
 public class Global {
 	public static int CLASS_NUMBER;
 	public static boolean sendFrame;
+	public static int token; //how many numbers 
+	public static int TOKENLIMIT = 1;
+	
+	//triggered transmission
+	public static int MOTIONTHRESH = 640 * 480 / 2;
 }
